180a6285af42f2bfcb0788136260b549a024beae
Alexander Dubovoi <a.v.dubovoj@gmail.com>
2020-01-12 23:59:52 +0200

    Added colored output

--- src/Main.java
+++ src/Main.java
@@ -6,19 +6,18 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Несколько");
-        System.out.println("строк");
-        System.out.println("для");
-        System.out.println("статистической");
-        System.out.println("информации");
-
+        // имя для файла с игровым полем
         String filename = "aquarium.txt";
         File file = new File(filename);
+
+        // проверяем наличие файла
         if (!file.exists()) {
+            // если не существует - пытаемся сгенерировать и записать
             try {
                 FileWriter fileWriter = new FileWriter(filename);
                 Random random = new Random();
 
+                // записываем на лету случайные числа в пределах индексов статических объектов
                 for (int i = 0; i < 20; i++) {
                     for (int j = 0; j < 80; j++) {
                         fileWriter.write(String.valueOf(random.nextInt(4)));
@@ -27,28 +26,41 @@ public class Main {
                         fileWriter.write("\n");
                     }
                 }
+
+                // закрываем поток записи
                 fileWriter.close();
             } catch (IOException e) {
                 e.printStackTrace();
             }
         }
 
+        // массив для инициализации игрового поля
         int[][] arr = new int[20][80];
+
+        // пытаемся прочитать файл, ведь он гарантированно существует
         try {
             FileReader fileReader = new FileReader(filename);
             Scanner scanner = new Scanner(fileReader);
+
+            // читаем посимвольно в массив
             for (int i = 0; i < 20; i++) {
                 char[] sym = scanner.nextLine().toCharArray();
                 for (int j = 0; j < sym.length; j++) {
                     arr[i][j] = Integer.parseInt(String.valueOf(sym[j]));
                 }
             }
+
+            // закрываем поток чтения
             fileReader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
+        // пытаемся получить экземпляр игрового поля, если его нет оно создается на основе прочтенного файла
+        // игровое поле может существовать только в елдиничном экземпляре, потому применен паттерн Singleton
         Aquarium aquarium = Aquarium.getInstance(arr);
+
+        // Выводим на экран данные игрового поля
         aquarium.showField();
     }
 }
--- src/obj_lib/Aquarium.java
+++ src/obj_lib/Aquarium.java
@@ -1,5 +1,7 @@
 package obj_lib;
 
+import java.io.IOException;
+
 public final class Aquarium {
     private static volatile Aquarium instance;
 
@@ -24,11 +26,32 @@ public final class Aquarium {
     }
 
     public void showField() {
+        // Стереть все что на экране
+        System.out.println("\033[2J");
+
+        // тут будут служебные строки
+        System.out.println("Несколько");
+        System.out.println("строк");
+        System.out.println("для");
+        System.out.println("статистической");
+        System.out.println("информации");
+
+        // форматируем и выводим поле
+        StringBuilder text = new StringBuilder();
+
         for (int[] line : this.field){
             for (int cell: line) {
-                System.out.print(cell + " ");
+                String fmt =  switch (cell) {
+                    case 0 -> "\033[34m";
+                    case 1 -> "\033[33m";
+                    case 2 -> "\033[32m";
+                    case 3 -> "\033[30;1m";
+                    default -> "\033[0m";
+                };
+                text.append(fmt).append(cell).append(" \033[0m");
             }
-            System.out.println();
+            text.append("\n");
         }
+        System.out.println(text);
     }
 }
